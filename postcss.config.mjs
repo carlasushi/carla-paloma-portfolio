002779d7da84fb87ae29c5636b32ec42064a4346
carla paloma <cpcodes@pm.me>
2024-08-22 11:51:24 -1000

    Update README.md and add project configuration files

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}, // Include autoprefixer for better cross-browser support
+
+  },
+};
+
+export default config;
